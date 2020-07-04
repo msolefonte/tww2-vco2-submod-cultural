643e5c0427b6391c80cd23a724b3e4adb536258b
Marc Solé Fonte <msolefonte@gmail.com>
2020-07-04 15:04:18 +0200

    cst, vmp, lzd, wef DONE

--- src/script/zzz_vco2_submod_cultural/factions/wh2_dlc11_cst_vampire_coast/campaigns/main_warhammer/missions.lua
+++ src/script/zzz_vco2_submod_cultural/factions/wh2_dlc11_cst_vampire_coast/campaigns/main_warhammer/missions.lua
@@ -2,145 +2,26 @@ local missions = {
     [[
  		mission
 		{
-			victory_type vco_victory_type_short;
+			victory_type vco_victory_type_cultural;
 			key wh_main_short_victory;
 			issuer CLAN_ELDERS;
 			primary_objectives_and_payload
 			{
 				objective
+                {
+                    type CONTROL_N_PROVINCES_INCLUDING;
+                    total 30;
+                }
+				objective
 				{
-					type HAVE_AT_LEAST_X_OF_A_POOLED_RESOURCE;
-					pooled_resource cst_infamy;
-					total 15000;
-					additive;
+					type RESEARCH_N_TECHS_OF_TYPE_X;
+					total 37;
 				}
 				objective
 				{
-					type CONTROL_N_REGIONS_FROM;
-					total 8;
-
-					region wh2_main_vampire_coast_the_awakening;
-					region wh2_main_vampire_coast_pox_marsh;
-					region wh2_main_vampire_coast_the_blood_swamps;
-					region wh2_main_volcanic_islands_the_star_tower;
-					region wh2_main_volcanic_islands_fuming_serpent;
-					region wh2_main_northern_great_jungle_temple_of_tlencan;
-					region wh2_main_headhunters_jungle_chupayotl;
-					region wh2_main_headhunters_jungle_mangrove_coast;
-					region wh2_main_the_creeping_jungle_temple_of_kara;
-					region wh2_main_southern_jungle_of_pahualaxa_the_high_sentinel;
-					region wh2_main_southern_jungle_of_pahualaxa_monument_of_the_moon;
-					region wh2_main_northern_jungle_of_pahualaxa_swamp_town;
-					region wh2_main_northern_jungle_of_pahualaxa_port_reaver;
-					region wh2_main_isthmus_of_lustria_skeggi;
-					region wh2_main_isthmus_of_lustria_ziggurat_of_dawn;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh2_main_lzd_hexoatl;
-					faction wh2_main_lzd_itza;
-					faction wh2_main_lzd_tlaxtlan;
-					faction wh2_dlc12_lzd_cult_of_sotek;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_BUILDINGS_FROM;
-					faction wh2_dlc11_cst_vampire_coast;
-					total 2;
-					building_level wh2_dlc11_special_ancient_vault_2;
-					building_level wh2_dlc11_special_ship_harkon_1;
-				}
-				objective
-				{
-					type OWN_N_UNITS;
-					total 60;
-				}
-				objective
-				{
-					override_text mission_text_text_mis_activity_restore_harkons_mind;
-                    type SCRIPTED;
-                    script_key restore_harkon_mind;
-				}
-
-				payload
-				{
-					game_victory;
-				}
-			}
-		}
-    ]],
-    [[
- 		mission
-		{
-			victory_type vco_victory_type_long;
-			key wh_main_long_victory;
-			issuer CLAN_ELDERS;
-			primary_objectives_and_payload
-			{
-				objective
-				{
-					type HAVE_AT_LEAST_X_OF_A_POOLED_RESOURCE;
-					pooled_resource cst_infamy;
-					total 25000;
-					additive;
-				}
-				objective
-				{
-					type CONTROL_N_REGIONS_FROM;
-					total 15;
-
-					region wh2_main_vampire_coast_the_awakening;
-					region wh2_main_vampire_coast_pox_marsh;
-					region wh2_main_vampire_coast_the_blood_swamps;
-					region wh2_main_volcanic_islands_the_star_tower;
-					region wh2_main_volcanic_islands_fuming_serpent;
-					region wh2_main_northern_great_jungle_temple_of_tlencan;
-					region wh2_main_headhunters_jungle_chupayotl;
-					region wh2_main_headhunters_jungle_mangrove_coast;
-					region wh2_main_the_creeping_jungle_temple_of_kara;
-					region wh2_main_southern_jungle_of_pahualaxa_the_high_sentinel;
-					region wh2_main_southern_jungle_of_pahualaxa_monument_of_the_moon;
-					region wh2_main_northern_jungle_of_pahualaxa_swamp_town;
-					region wh2_main_northern_jungle_of_pahualaxa_port_reaver;
-					region wh2_main_isthmus_of_lustria_skeggi;
-					region wh2_main_isthmus_of_lustria_ziggurat_of_dawn;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh2_dlc11_cst_noctilus;
-					faction wh2_dlc11_cst_the_drowned;
-					faction wh2_dlc11_cst_pirates_of_sartosa;
-
-
-					faction wh2_main_lzd_itza;
-					faction wh2_dlc12_lzd_cult_of_sotek;
-					faction wh2_main_lzd_sentinels_of_xeti;
-					faction wh2_main_lzd_hexoatl;
-					faction wh2_main_lzd_tlaxtlan;
-					faction wh2_dlc13_lzd_spirits_of_the_jungle;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_BUILDINGS_FROM;
-					faction wh2_dlc11_cst_vampire_coast;
-					total 2;
-					building_level wh2_dlc11_special_ancient_vault_2;
-					building_level wh2_dlc11_special_ship_harkon_1;
-				}
-				objective
-				{
-					type OWN_N_UNITS;
-					total 100;
-				}
-				objective
-				{
-					override_text mission_text_text_mis_activity_restore_harkons_mind;
-                    type SCRIPTED;
-                    script_key restore_harkon_mind;
+					type AT_LEAST_X_RELIGION_IN_ALL_PROVINCES;
+					total 51;
+					religion wh_main_religion_undeath;
 				}
 
 				payload
--- src/script/zzz_vco2_submod_cultural/factions/wh_main_vmp_vampire_counts/campaigns/main_warhammer/missions.lua
+++ src/script/zzz_vco2_submod_cultural/factions/wh_main_vmp_vampire_counts/campaigns/main_warhammer/missions.lua
@@ -2,56 +2,26 @@ local missions = {
     [[
  		mission
 		{
-			victory_type vco_victory_type_short;
+			victory_type vco_victory_type_cultural;
 			key wh_main_short_victory;
 			issuer CLAN_ELDERS;
 			primary_objectives_and_payload
 			{
 				objective
+                {
+                    type CONTROL_N_PROVINCES_INCLUDING;
+                    total 30;
+                }
+				objective
 				{
-					type CONTROL_N_PROVINCES_INCLUDING;
-					total 12;
+					type RESEARCH_N_TECHS_OF_TYPE_X;
+					total 37;
 				}
 				objective
 				{
-					type DESTROY_FACTION;
-					faction wh_main_vmp_schwartzhafen;
-					confederation_valid;
-				}
-
-				payload
-				{
-					game_victory;
-				}
-			}
-		}
-    ]],
-    [[
- 		mission
-		{
-			victory_type vco_victory_type_long;
-			key wh_main_long_victory;
-			issuer CLAN_ELDERS;
-			primary_objectives_and_payload
-			{
-				objective
-				{
-					type CONTROL_N_PROVINCES_INCLUDING;
-					total 20;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh_main_vmp_schwartzhafen;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_BUILDINGS_FROM;
-					faction wh_main_vmp_vampire_counts;
-					total 2;
-					building_level wh2_dlc09_special_settlement_pyramid_of_nagash_vmp_5;
-					building_level wh2_main_special_pyramid_of_nagash_vmp;
+					type AT_LEAST_X_RELIGION_IN_ALL_PROVINCES;
+					total 51;
+					religion wh_main_religion_undeath;
 				}
 
 				payload
