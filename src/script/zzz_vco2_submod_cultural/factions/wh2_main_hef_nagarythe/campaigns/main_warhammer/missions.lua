b401cf317a4dcb0656cedc6bc342ed6594f8d347
Marc Solé Fonte <msolefonte@gmail.com>
2020-07-04 15:09:00 +0200

    GRN, HEF

--- src/script/zzz_vco2_submod_cultural/factions/wh2_main_hef_nagarythe/campaigns/main_warhammer/missions.lua
+++ src/script/zzz_vco2_submod_cultural/factions/wh2_main_hef_nagarythe/campaigns/main_warhammer/missions.lua
@@ -2,92 +2,26 @@ local missions = {
     [[
  		mission
 		{
-			victory_type vco_victory_type_short;
+			victory_type vco_victory_type_cultural;
 			key wh_main_short_victory;
 			issuer CLAN_ELDERS;
 			primary_objectives_and_payload
 			{
 				objective
+                {
+                    type CONTROL_N_PROVINCES_INCLUDING;
+                    total 30;
+                }
+				objective
 				{
-					type CONTROL_N_PROVINCES_INCLUDING;
-					total 2;
-					province wh2_main_nagarythe;
-					province wh2_main_iron_mountains;
-					province wh2_main_titan_peaks;
-					province wh2_main_the_road_of_skulls;
-					province wh2_main_doom_glades;
+					type RESEARCH_N_TECHS_OF_TYPE_X;
+					total 53;
 				}
 				objective
 				{
-					type DESTROY_FACTION;
-					faction wh2_main_def_naggarond;
-					faction wh2_main_def_bleak_holds;
-					faction wh2_main_def_clar_karond;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_BUILDINGS_FROM;
-					faction wh2_main_hef_nagarythe;
-					total 1;
-					building_level wh2_main_special_anlec_citadel_hef;
-				}
-				objective
-				{
-					type OWN_N_UNITS;
-					total 2;
-					additive;
-					unit wh2_main_hef_mon_moon_dragon;
-				}
-
-				payload
-				{
-					game_victory;
-				}
-			}
-		}
-    ]],
-    [[
- 		mission
-		{
-			victory_type vco_victory_type_long;
-			key wh_main_long_victory;
-			issuer CLAN_ELDERS;
-			primary_objectives_and_payload
-			{
-				objective
-				{
-					type CONTROL_N_PROVINCES_INCLUDING;
-					total 4;
-					province wh2_main_nagarythe;
-					province wh2_main_iron_mountains;
-					province wh2_main_titan_peaks;
-					province wh2_main_the_road_of_skulls;
-					province wh2_main_doom_glades;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh2_main_def_naggarond;
-					faction wh2_main_def_cult_of_pleasure;
-					faction wh2_main_def_har_ganeth;
-					faction wh2_main_def_bleak_holds;
-					faction wh2_main_def_clar_karond;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_BUILDINGS_FROM;
-					faction wh2_main_hef_nagarythe;
-					total 1;
-					building_level wh2_main_special_anlec_citadel_hef;
-				}
-				objective
-				{
-					type OWN_N_UNITS;
-					total 3;
-					additive;
-					unit wh2_main_hef_mon_moon_dragon;
+					type AT_LEAST_X_RELIGION_IN_ALL_PROVINCES;
+					total 51;
+					religion wh_main_religion_untainted;
 				}
 
 				payload
--- src/script/zzz_vco2_submod_cultural/factions/wh2_main_hef_order_of_loremasters/campaigns/main_warhammer/missions.lua
+++ src/script/zzz_vco2_submod_cultural/factions/wh2_main_hef_order_of_loremasters/campaigns/main_warhammer/missions.lua
@@ -2,7 +2,7 @@ local missions = {
     [[
  		mission
 		{
-			victory_type vco_victory_type_short;
+			victory_type vco_victory_type_cultural;
 			key wh_main_short_victory;
 			issuer CLAN_ELDERS;
 			primary_objectives_and_payload
@@ -10,103 +10,18 @@ local missions = {
 				objective
                 {
                     type CONTROL_N_PROVINCES_INCLUDING;
-                    total 1;
-					province wh2_main_saphery;
-                }
-                objective
-				{
-					type CONTROL_N_REGIONS_INCLUDING;
-					total 12;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh2_dlc11_cst_vampire_coast;
-
-					faction wh2_dlc11_def_the_blessed_dread;
-
-					faction wh2_main_skv_clan_pestilens;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_OF_A_BUILDING;
-					faction wh2_main_hef_order_of_loremasters;
-					total 1;
-					building_level wh2_main_special_tower_of_hoeth_2;
-				}
-				objective
-				{
-					type CONSTRUCT_N_OF_A_BUILDING;
-					faction wh2_main_hef_order_of_loremasters;
-					total 1;
-					building_level wh2_main_hef_mages_3;
-				}
-
-				payload
-				{
-					game_victory;
-				}
-			}
-		}
-    ]],
-    [[
- 		mission
-		{
-			victory_type vco_victory_type_long;
-			key wh_main_long_victory;
-			issuer CLAN_ELDERS;
-			primary_objectives_and_payload
-			{
-				objective
-                {
-                    type CONTROL_N_PROVINCES_INCLUDING;
-                    total 1;
-					province wh2_main_saphery;
-                }
-                objective
-				{
-					type CONTROL_N_REGIONS_INCLUDING;
-					total 20;
-				}
-				objective
-				{
-					type DESTROY_FACTION;
-					faction wh2_dlc11_cst_vampire_coast;
-					faction wh2_dlc11_cst_noctilus;
-
-					faction wh2_dlc11_def_the_blessed_dread;
-
-					faction wh2_main_skv_clan_pestilens;
-
-					faction wh2_dlc09_tmb_followers_of_nagash;
-					confederation_valid;
-				}
-				objective
-				{
-					type CONSTRUCT_N_OF_A_BUILDING;
-					faction wh2_main_hef_order_of_loremasters;
-					total 1;
-					building_level wh2_main_special_tower_of_hoeth_2;
-				}
-				objective
-				{
-					type CONSTRUCT_N_OF_A_BUILDING;
-					faction wh2_main_hef_order_of_loremasters;
-					total 3;
-					building_level wh2_main_hef_mages_3;
-				}
-				objective
-                {
-					override_text mission_text_text_mis_activity_archaon_spawned;
-                    type SCRIPTED;
-                    script_key archaon_spawned;
+                    total 30;
                 }
 				objective
 				{
-					type HAVE_CHARACTER_WOUNDED;
-					override_text mission_text_text_wh_main_objective_override_archaon_wounded;
-					start_pos_character 2140782858;
+					type RESEARCH_N_TECHS_OF_TYPE_X;
+					total 53;
+				}
+				objective
+				{
+					type AT_LEAST_X_RELIGION_IN_ALL_PROVINCES;
+					total 51;
+					religion wh_main_religion_untainted;
 				}
 
 				payload
